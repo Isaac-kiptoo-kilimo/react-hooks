73a31fcfa72dc5c73fd007cadc3f44bfce5a5b0d
unknown <isaackilimok2@gmail.com.com>
2024-01-11 11:50:45 +0300

    feat:added the use of useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function initialFunction(){
-  console.log("This function is running");
-  return 4
-}
+
  function App(){
 
-  const[count,setCount]=useState(()=>initialFunction());
+  const[count,setCount]=useState(4);
+  const[theme,setTheme]=useState("blue");
+  
+  const[resourceType, setResourceType]=useState('posts')
 
   function decreamentCount(){
-    setCount(prevCount=>prevCount-1)
-
+   setCount(prevCount=>prevCount-1)
+   setTheme("green")
   }
 
   function incrementCount(){
-    setCount(prevCount=>prevCount+1)
+   setCount(prevCount=>prevCount+1)
+   setTheme("red")
   }
+
+  useEffect(()=>{
+    console.log("Its getting rendered");
+
+  })
   return(
     <>
+    <div className="count">
     <button onClick={decreamentCount}>-</button>
     <button>{count}</button>
+    <button>{theme}</button>
     <button onClick={incrementCount}>+</button>
+    </div>
+
+    <div className="comments">
+    <button onClick={()=>setResourceType('posts')}>Posts</button>
+    <button onClick={()=>setResourceType('users')}>users</button>
+    <button onClick={()=>setResourceType('comments')}>Comments</button>
+    <h4>{resourceType}</h4>
+    </div>
+
     </>
   )
  }
